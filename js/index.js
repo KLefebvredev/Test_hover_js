bcbae9331348184c990636e04febdfa9560d5f44
klefebvre <klefebvre.dev@gmail.com>
2021-04-17 12:16:43 +0200

    test swup

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
-let hello = "hello"
-console.log(hello);
+const posi = document.documentElement;
+const swup = new Swup() // const transition entre les pages gràce à la lib swup
+
+posi.addEventListener("mousemove",event => {
+    posi.style.setProperty('--x',event.clientX + "px");
+    posi.style.setProperty('--y',event.clientY + "px");
+})
